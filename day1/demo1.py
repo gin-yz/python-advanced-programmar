0edfa74a8853b39df22bce1f06e2f2ef77d1ca79
gin_yz <ginyz@foxmail.com>
2020-04-25 00:13:35 +0800

    第一天学习

--- day1/demo1.py
+++ day1/demo1.py
@@ -1,1 +1,13 @@
-print(1)
+class Company(object):
+    def __init__(self, list_demo):
+        self.list_demo = list_demo
+
+    def __str__(self):
+        return ",".join(self.list_demo)
+
+    def __repr__(self):
+        return ",".join(self.list_demo)
+
+
+company = Company(["1", '2', '3'])
+print(company)
