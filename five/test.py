f96012731e9a017eea6d5c790e617356f681aee2
gin-yz <ginyz@foxmail.com>
2020-06-11 04:02:14 +0800

    6月11号更新

---
+++ five/test.py
@@ -0,0 +1,10 @@
+dict_str = {'1': '2',}
+dict_str.pop('1')
+print(dict_str)
+
+list_test = [1,2,3,4]
+list_test[1:1] = [5]
+print(list_test)
+
+b = sorted(list_test,reverse=True)
+print(b)
---
+++ four/test.py
@@ -0,0 +1,3 @@
+x,y,z = list([1,2,3])
+
+print(x,y,z)
