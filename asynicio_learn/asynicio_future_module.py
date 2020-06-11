1045f3e92ccf1088bc4e6efd4fd95994be6d7507
gin-yz <ginyz@foxmail.com>
2020-06-11 23:09:35 +0800

    asyncio协程编程

---
+++ asynicio_learn/asynicio_future_module.py
@@ -0,0 +1,24 @@
+# future是一个底层级的api，负责循环等待
+import asyncio
+
+async def set_after(future):
+    await asyncio.sleep(2)
+    future.set_result('return code')
+
+async def main():
+    loop = asyncio.get_running_loop()
+
+    future = loop.create_future()
+
+    print(future)
+
+    await loop.create_task(set_after(future))
+
+    return_data = await future
+
+    print(future)
+
+    print(return_data)
+
+if __name__ == '__main__':
+    asyncio.run(main())
